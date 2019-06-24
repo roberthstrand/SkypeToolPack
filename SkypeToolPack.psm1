5a55cf4783ca3dd8d72abf5845f88bba1ed02960
Roberth Strand <roberth@strandkonsult.no>
2019-06-24 13:36:59 +0200

    Initial commit, new proxy function

--- SkypeToolPack.psm1
+++ SkypeToolPack.psm1
@@ -1,3 +1,4 @@
+. $PSScriptRoot\src\Get-CsProxyAddress.ps1
 function Get-CsPoolService {
     <#
     .SYNOPSIS
@@ -190,5 +191,26 @@ function Get-CsProxyAddress {
             Write-Error "SIP & SMTP does not match!"
         }
     }
+}
+function Test-CsDnsRecords {
+    param (
+        [Parameter(Position = 0,
+            Mandatory, HelpMessage = "SIP domain you want to check")]
+        [string]$SipDomain
+    )
+    #Test whether internal or external
+    if ((Resolve-DnsName lyncdiscoverinternal.$SipDomain -ErrorAction SilentlyContinue) -and (Resolve-DnsName $SipDomain)) {
+        $environment = "internal"
+    } elseif (!(Resolve-DnsName $SipDomain -ErrorAction SilentlyContinue)) {
+        Write-Error "DNS name does not exist"
+    } else {
+        $environment = "external"
+    }
+    #Sjekk offentlig DNS om det er external, eller begge delene om det er internal.
+    <#
+    Internal:
+    Lyncdiscoverinternal.$SipDomain, hent ut access edge
+    access.$SipDomain basert på LDI
+    #>
 }
 Export-ModuleMember -Cmdlet Get-CsPoolService, Restart-CsPoolService, Get-CsResponseGroupService, Get-CsProxyAddress -Function Get-CsPoolService, Restart-CsPoolService, Get-CsResponseGroupService, Get-CsProxyAddress
---
+++ src/Get-CsProxyAddress.ps1
@@ -0,0 +1,44 @@
+function Get-CsProxyAddress {
+    <#
+    .SYNOPSIS
+    Checks a user for matching SIP and primary SMTP address.
+    .DESCRIPTION
+    Checks a user for matching SIP and primary SMTP address.
+    .PARAMETER SAMAccount
+    Required, defines the user to check. This gather all the proxyaddresses that has been defined in Active Directory, then find the SIP and primary SMTP to match later.
+    .EXAMPLE
+    Get-CsProxyAddress SAMAccountName
+    #>
+    Param (
+        [Parameter(
+            Position = 0, ParameterSetName = "SingleUser", Mandatory, HelpMessage = "Users SAMAccountName.")]
+        [string]$SAMAccount,
+        [Parameter(
+            ParameterSetName = "Everyone", HelpMessage = "Switch to check your entire Skype installation for discrepancy.")]
+        [switch]$FullScan
+    )
+    function proxyExtract {
+        $sip = $null
+        $smtp = $null
+        foreach ($proxy in ($user).ProxyAddresses) {
+            if ($proxy -like "sip:*") {
+                $sip = $proxy.substring(4)
+            }
+            if ($proxy -clike "SMTP:*") {
+                $smtp = $proxy.substring(5)
+            }
+        }
+        $result = [PSCustomObject]@{
+            sip = $sip
+            smtp = $smtp
+        }
+        return $result
+    }
+    if ($FullScan) {
+
+    } else {
+        $user = Get-AdUser $SAMAccount -properties ProxyAddresses,msRTCSIP-PrimaryUserAddress
+        $proxy = proxyExtract
+        Write-Output $proxy
+    }
+}
